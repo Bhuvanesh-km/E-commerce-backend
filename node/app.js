4a9dfbc8525ad4f2d99fe7457c2d8411ede17af1
Bhuvanesh-km <bhuvaneshkm441@gmail.com>
2024-03-04 13:02:08 +0530

    updated cors

--- node/app.js
+++ node/app.js
@@ -21,8 +21,8 @@ app.use(cookieParser());
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-  standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  // standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  // legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
   // store: ... , // Use an external store for consistency across multiple server instances.
 });
 // Apply the rate limiting middleware to all requests.
@@ -34,7 +34,7 @@ app.use(mongoSanitize());
 // app.use(cors());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: true,
     credentials: true,
   })
 );
